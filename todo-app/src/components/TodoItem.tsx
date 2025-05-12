03d01f2aa194273d655dc5510bf43574f9e7b40b
55848734 <shantanutripathi96@gmail.com>
2025-05-12 10:23:20 +0000

    checkpoint @ 47

---
+++ todo-app/src/components/TodoItem.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Todo } from '@/types';
+
+interface TodoItemProps {
+  todo: Todo;
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggle, onDelete }) => {
+  return (
+    <li className="flex items-center justify-between p-4 border-b border-gray-200">
+      <div className="flex items-center">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => onToggle(todo.id)}
+          className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+        />
+        <span 
+          className={`ml-3 ${todo.completed ? 'line-through text-gray-500' : ''}`}
+        >
+          {todo.title}
+        </span>
+      </div>
+      <button
+        onClick={() => onDelete(todo.id)}
+        className="text-red-500 hover:text-red-700 focus:outline-none"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+        </svg>
+      </button>
+    </li>
+  );
+};
+
+export default TodoItem;
