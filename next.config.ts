6ed326842345d3efe8a588ad0c27401329242899
55848734 <shantanutripathi96@gmail.com>
2025-05-12 10:12:46 +0000

    checkpoint @ 16

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: '/app',
-  assetPrefix: '/app', // 
-};
-
-export default nextConfig;
